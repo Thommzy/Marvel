60674f31da7d375223188e9917f16709a93d1b9b
Timothy Obeisun <timothy.obeisun@edukoya.com>
2022-05-21 10:06:00 +0100

    remove storyboard and added swiftLint

---
+++ WallapopMarvel/WallapopMarvel/Scenes/Home/HomeVC.swift
@@ -0,0 +1,15 @@
+//
+//  HomeVC.swift
+//  WallapopMarvel
+//
+//  Created by Timothy  on 21/05/2022.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+}
