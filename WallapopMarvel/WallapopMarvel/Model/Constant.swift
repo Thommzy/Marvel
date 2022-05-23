f21dd64009ac614d1e286e0c7efa77a0ba90951e
Timothy Obeisun <timothy.obeisun@edukoya.com>
2022-05-23 15:09:03 +0100

    created constant file and added Combine to VC

--- WallapopMarvel/WallapopMarvel/Model/Constant.swift
+++ WallapopMarvel/WallapopMarvel/Model/Constant.swift
@@ -9,6 +9,9 @@ import Foundation
 
 enum Config {
     static let baseEndPoint = "https://gateway.marvel.com"
+    static let apiKey = "0702af7759fed52f09d5becfe3156deb"
+    static let hash = "78e72675f178d55e3911ed7c730851f7"
+    static let timeStamp = 1653117571321
 }
 
 enum Versions {
--- WallapopMarvel/WallapopMarvel/Scenes/Home/HomeVC/HomeVC.swift
+++ WallapopMarvel/WallapopMarvel/Scenes/Home/HomeVC/HomeVC.swift
@@ -16,14 +16,12 @@ class HomeVC: UIViewController {
     weak var homeCoordinateDelegate: HomeVCDelegate?
     var viewModel: HomeViewModelling!
     private var subscriptions = Set<AnyCancellable>()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
         setupPreData()
         bind(to: viewModel)
     }
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         viewModel.viewDidAppear.send()
@@ -32,14 +30,11 @@ class HomeVC: UIViewController {
 
 private extension HomeVC {
     func bind(to viewModel: HomeViewModelling) {
-        subscriptions = [
-        
-        ]
+        subscriptions = []
     }
-    
     func setupPreData() {
-        viewModel.timeStamp.send(1653117571321)
-        viewModel.apiKey.send("0702af7759fed52f09d5becfe3156deb")
-        viewModel.hash.send("78e72675f178d55e3911ed7c730851f7")
+        viewModel.timeStamp.send(Config.timeStamp)
+        viewModel.apiKey.send(Config.apiKey)
+        viewModel.hash.send(Config.hash)
     }
 }
